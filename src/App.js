3382170296abab1f33a62f658610a5853b8a5089
Francisco Ponce <francisco.eugp@gmail.com>
2020-11-19 00:45:51 -0600

    uggly countdown

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React, { useState, useEffect } from 'react'
+
+
+const App = () => {
+  const [seconds, setSeconds] = useState(0)
+  const [minutes, setMinutes] = useState(15)
+
+  const timeRunner = () => {
+    if (minutes == 0 && seconds == 0) {
+      setSeconds(0)
+      setMinutes(15)
+
+    }
+    else {
+      if (seconds == 0) {
+        setMinutes(minutes => minutes - 1)
+        setSeconds(59)
+      }
+      else {
+        setSeconds(seconds=>seconds-1)
+      }
+    }
+  }
+
+  useEffect(() => {
+    const token = setTimeout(timeRunner, 1000)
+    return function reset () {
+      return clearTimeout(token)
+    }
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      <span>{minutes}:{seconds}</span>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
